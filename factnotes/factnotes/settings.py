dbc91f6789a31bb50d9dcd3f3a998578f1b55425
Tathya Dixit <dixit.tathya2003@gmail.com>
2024-07-03 20:29:51 +0530

    settings commit

--- factnotes/factnotes/settings.py
+++ factnotes/factnotes/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_)ux^8yj2m2@vh-^&7p5=x==i1-t&pa6&+wyi6d@gjkcqvz#3o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
