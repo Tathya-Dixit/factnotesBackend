b27d08af9379a6c266073736daf1729e629c1b5e
Tathya Dixit <dixit.tathya2003@gmail.com>
2024-07-05 13:03:59 +0530

    postgres commit

--- factnotes/factnotes/settings.py
+++ factnotes/factnotes/settings.py
@@ -109,8 +109,12 @@ WSGI_APPLICATION = 'factnotes.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'rPNcohnrgOSiGbMmuWHvoyinazVzjWLf',
+        'HOST': 'viaduct.proxy.rlwy.net',
+        'PORT': '54465',
     }
 }
 
